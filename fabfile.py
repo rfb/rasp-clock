b9ec9c67c8fe9884b3e832a2fd06928631312b32
Ryan Barber <rfb@skyscraper.nu>
2014-03-08 12:41:53 -0800

    first stab at a fabfile

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import *
+
+env.hosts = [ 'raspi' ]
+
+def pack():
+  local('python setup.py sdist --formats=gztar', capture=False)
+
+def deploy():
+  dist = local('python setup.py --fullname', capture=True).strip()
+  put('dist/%s.tar.gz' % dist, '/tmp/clock.tar.gz')
+
+  with cd('/tmp'):
+    run('tar xzf /tmp/clock.tar.gz')
+
+  with cd('/tmp/%s' % dist):
+    run('sudo python setup.py install')
